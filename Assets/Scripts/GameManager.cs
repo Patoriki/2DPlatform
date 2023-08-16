a725c98f826895f8fe2330ecb3647d1b9552e147
Patoriki <142239657+Patoriki@users.noreply.github.com>
2023-08-16 08:44:47 -0300

    add menu e iteracao do game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EVO.Core.Singleton;
+using DG.Tweening;
+
+public class GameManager : Singleton<GameManager>
+{
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+
+    [Header("Animation")]
+    public float duration = 0.5f;
+    public float delay = 0.1f;
+    public Ease ease = Ease.OutBack;
+
+    private GameObject _currentPlayer;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From().SetDelay(delay);
+    }
+}
