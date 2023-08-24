7d1ed4c1d3f171ee17bc9ac71760546bbc20bb1c
Patoriki <142239657+Patoriki@users.noreply.github.com>
2023-08-24 12:37:36 -0300

    add collactable system

---
+++ Assets/Scripts/Collactable/ItemCollactableBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ItemCollactableBase : MonoBehaviour
+{
+    [Header("Animation Setup")]
+    public float jumpScaleY = 0;
+    public float jumpScaleX = 0;
+    public float animationJumpDuration = 0.3f;
+    public Ease ease = Ease.OutBack;
+    public string compareTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag(compareTag))
+        {
+            Collect();
+        }
+    }
+
+    protected virtual void Collect()
+    {
+        HandleScale();
+        StartCoroutine(DestroyCoin());
+        OnCollect();
+    }
+
+    protected virtual void OnCollect()
+    {
+
+    }
+
+    private void HandleScale()
+    {
+        transform.DOScaleY(jumpScaleY, animationJumpDuration).SetEase(ease);
+        transform.DOScaleX(jumpScaleX, animationJumpDuration).SetEase(ease);
+    }
+
+    private IEnumerator DestroyCoin()
+    {
+        yield return new WaitForSeconds(0.3f);
+        gameObject.SetActive(false);
+    }
+}
