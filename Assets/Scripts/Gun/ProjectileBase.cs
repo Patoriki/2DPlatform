cae5cb193519991df4bede329e72738779738067
Patoriki <142239657+Patoriki@users.noreply.github.com>
2023-08-25 15:24:47 -0300

    add projectile and enemy damage

--- Assets/Scripts/Gun/ProjectileBase.cs
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ProjectileBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 direction;
+
+    public float timeToDestroy = 2f;
+
+    public float side = 1f;
+
+    public int damageAmount = 1;
+
+    private void Awake()
     {
-        
+        Destroy(gameObject, timeToDestroy);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * Time.deltaTime * side);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var enemy = collision.transform.GetComponent<EnemyBase>();
+
+        if(enemy != null)
+        {
+            enemy.Damage(damageAmount);
+            Destroy(gameObject);
+        }
     }
 }
